36d7370815669bd09bf16048616a6b373c3e5733
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-11-15 20:17:43 -0500

    update requirements.txt

--- python/app/testExtraction.py
+++ python/app/testExtraction.py
@@ -1,4 +1,4 @@
-from services.extraction import extract_web_article
+from services.webArticleExtraction import extract_web_article
 
 result = extract_web_article("https://en.wikipedia.org/wiki/Python_(programming_language)")
 print(result["title"])
